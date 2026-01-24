c86f7ad80d28f0bd1fe186160c7c0c4ada0b15d9
Naywaz Sharif <naywaz.sh@gmail.com>
2026-01-24 03:09:57 +0000

    codes upd

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Sidebar from "./components/Sidebar"; // Import the sidebar
+import Sidebar from "../components/Sidebar"; // Import the sidebar
 
 const inter = Inter({ subsets: ["latin"] });
 
